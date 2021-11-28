f53d3935754285e4954322a7887c773e185ecf6d
Maxime Amodei <maxime.amodei@gmail.com>
2021-11-28 15:48:22 +0100

    check UUID in checker

--- pims_plugin_format_msi/imzml/checker.py
+++ pims_plugin_format_msi/imzml/checker.py
@@ -2,10 +2,52 @@
 
 from __future__ import annotations
 
-import warnings
+import re
 
+from pims.files.file import Path
 from pims.formats.utils.abstract import CachedDataPath
 from pims.formats.utils.checker import AbstractChecker
+
+from .utils import get_imzml_pair
+
+IMZML_UUID_ACCESSOR = 'IMS:1000080'
+
+
+def check_uuid(imzml_path: Path, ibd_path: Path) -> bool:
+    """check_uuid: verify that a pair of imzML & ibd files have the same UUID
+
+    Args:
+        imzml_path (Path): path to the imzML file
+        ibd_path (Path): path tot the ibd file
+
+    Returns:
+        bool: the equivalence of the UUID
+
+    """
+
+    try:
+        # get binary UUID
+        with open(ibd_path, mode='rb') as ibd:
+            ibd_uuid = ibd.read(16).hex()
+
+        # get imzML UUID
+        with open(imzml_path, mode='r', encoding='utf-8') as imzml:
+            for line in imzml:
+                # re.match caches pattern so no need to worry about compiling RE
+                if re.match(rf'.*{IMZML_UUID_ACCESSOR}.*', line):
+
+                    # find UUID
+                    imzml_uuid_lst = re.findall(r'value="{?(.*)}?"', line)
+                    if len(imzml_uuid_lst) != 1:
+                        return False
+
+                    # remove hyphens
+                    imzml_uuid = imzml_uuid_lst[0].replace('-', '')
+
+        return imzml_uuid == ibd_uuid
+
+    except (Exception):  # TODO which exception could be thrown ?
+        return False
 
 
 class ImzMLChecker(AbstractChecker):
@@ -15,43 +57,9 @@ class ImzMLChecker(AbstractChecker):
     def match(cls, pathlike: CachedDataPath) -> bool:
         """Whether the path is in this format or not."""
 
-        # requires a directory with .imzML and .ibd
-        if not pathlike.path.is_dir():
+        pair = get_imzml_pair(pathlike.path)
+
+        if pair is None:
             return False
 
-        # get a list of files in the directory
-        files = [f for f in pathlike.path.iterdir() if f.is_file()]
-        files_as_str = [str(f).lower() for f in files]
-
-        # get all files ending in .imzML (case insensitive)
-        imzml_files = [f for f in files if f.suffix.lower() == '.imzml']
-
-        if not imzml_files:
-            return False
-
-        if len(imzml_files) > 1:
-            warnings.warn('More than 1 imzML file were uploaded')
-
-        for imzml in imzml_files:
-            target = imzml.with_suffix('.ibd')
-            target_as_str = str(target).lower()
-
-            # check if target exists (case sensitive)
-            if target in files:
-                return True
-
-            # check for case insensitive file
-            if target_as_str not in files_as_str:
-                continue
-            idx = files_as_str.index(target_as_str)
-
-            # check for name without extension
-            if imzml.with_suffix('') == files[idx].with_suffix(''):
-                return True
-
-            # TODO allow case mis-match ?
-            # maybe check the UUID in the file if needed
-            #   - easy to get in the .ibd
-            #   - need to parse imzml ?
-
-        return False
+        return check_uuid(*pair)
